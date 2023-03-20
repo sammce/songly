baa288515b0c0dd61fec8e41f9521a2e0940d0c2
Sam McElligott <sammcelligott@outlook.com>
2023-03-20 23:19:05 +0000

    chore: commit prior to messing with next version

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head';
 import classes from './index.module.css';
-import Button from '@/components/Button';
+import { Canvas } from '@react-three/fiber';
 
 export default function Home() {
   return (
@@ -14,22 +14,15 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={classes.main}>
-        <h1>Primary</h1>
-        <div className={classes.flex}>
-          <Button>Filled</Button>
-          <Button variant="outline">Outline</Button>
-          <Button variant="ghost">Ghost</Button>
-        </div>
-        <h1>Highlight</h1>
-        <div className={classes.flex}>
-          <Button color="highlight">Filled</Button>
-          <Button variant="outline" color="highlight">
-            Outline
-          </Button>
-          <Button variant="ghost" color="highlight">
-            Ghost
-          </Button>
+      <main className={classes.container}>
+        <div className={classes.hero}>
+          <h1 className={classes.title}>Songly</h1>
+          <Canvas>
+            <mesh>
+              <boxGeometry />
+              <meshStandardMaterial />
+            </mesh>
+          </Canvas>
         </div>
       </main>
     </>
