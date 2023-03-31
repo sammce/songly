0063c3bf8bd7b1e9937930a9f3b10c5907cbdd35
Sam McElligott <sammcelligott@outlook.com>
2023-03-31 15:30:10 +0100

    feat: improve mobile layout, add github link

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,4 +1,7 @@
+import Button from '../Button';
 import classes from './Footer.module.css';
+import { BsGithub } from 'react-icons/bs';
+import Link from 'next/link';
 
 const Footer = () => {
   const year = new Date().getFullYear();
@@ -9,6 +12,10 @@ const Footer = () => {
           © <span className="gradient-text">Songly</span> {year} <br />
           CA115 Group R
         </p>
+        <div className={classes.verticalRule}></div>
+        <Link target="_blank" href="https://github.com/sammce/songly.git">
+          <BsGithub color="var(--text-primary)" size={42} />
+        </Link>
       </footer>
     </div>
   );
